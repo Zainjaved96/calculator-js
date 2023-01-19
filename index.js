9a2726e427be38d95208c58056c1514f4c642fa8
Zain <zainjaved965@gmail.com>
2023-01-19 14:49:52 +0500

    finished and dark mode added

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 let screen = document.getElementById("screen")
 var buttons = document.getElementsByClassName("btn")
+let output_var = "radian"
 
+function modeswitch(){
+    if (document.getElementById("body").classList.contains("light")){
+        document.getElementById("body").classList.remove("light")
+        document.getElementById("mode").innerText = "Light"
+        document.getElementById("cal-head").classList.remove("black-text")
+       
+    }
+    else {
+        document.getElementById("body").classList.add("light")
+        document.getElementById("mode").innerText = "Dark"
+        document.getElementById("cal-head").classList.add("black-text")
+    }
+}
+
+
+
+
+function deg_rad (){
+    let value = document.getElementById("radian")
+    if (value.innerText == "Rad"){
+        value.innerText = "Deg"
+        output_var = "degree"
+        console.log("degree enabled")
+    }
+    else {
+        value.innerText = "Rad"
+        output_var = "radian"
+        console.log("radian enabled")
+    }
+}
+
+function radians_to_degrees(radians)
+{
+  var pi = Math.PI;
+  return radians * (180/pi);
+}
 
 function float_round(value){
     let float_value = parseFloat(value)
@@ -40,8 +77,14 @@ function sin(){
     if (document.getElementById("sin").innerText == "sin"){
         screen.value = Math.sin(screen.value)
         screen.value = float_round(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
     }else {
         screen.value = Math.asin(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
         screen.value = float_round(screen.value)
     }
 }
@@ -49,11 +92,17 @@ function sin(){
 function cos(){
     if (document.getElementById("cos").innerText == "cos"){
         screen.value = Math.cos(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
         screen.value = float_round(screen.value)
     }
     
     else {
         screen.value = Math.acos(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
         screen.value = float_round(screen.value)
     }
     
@@ -62,10 +111,16 @@ function cos(){
 function tan(){
     if (document.getElementById("tan").innerText == "tan"){
         screen.value = Math.tan(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
         screen.value = float_round(screen.value)
     }
     else {
         screen.value = Math.atan(screen.value)
+        if(output_var == "degree"){
+            screen.value = radians_to_degrees(screen.value)
+        }
         screen.value = float_round(screen.value)
     }
 
@@ -130,4 +185,22 @@ function inverse(){
         document.getElementById("cos").innerText = "cos"
         document.getElementById("inverse").classList.remove("enabled")
     }
+}
+
+
+function fact() {
+    var i, num, f;
+    f = 1
+    num = screen.value;
+    for (i = 1; i <= num; i++) {
+        f = f * i;
+    }
+
+    i = i - 1;
+
+    screen.value = f;
+}
+
+function ce(){
+    screen.value = screen.value.substr(0, screen.value.length - 1);
 }
